6fff706feac0235447ca13a778aec3349d40c9d3
zhangjia <1569241790@qq.com>
2022-01-25 21:36:25 +0800

    feat: 整理项目结构， 启动项目

--- platform-main/src/main/java/com/example/platform/main/PlatformMainApplication.java
+++ platform-main/src/main/java/com/example/platform/main/PlatformMainApplication.java
@@ -1,4 +1,4 @@
-package com.example.platformmain;
+package com.example.platform.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
